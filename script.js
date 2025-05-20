a8897974d158c714e09790968345fa4a1da5b75e
Nightt630 <artemribakov6@gmail.com>
2025-05-20 16:15:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let batterylvl =29;
+
+
+
+if (batterylvl = 99){
+
+    console.log("Ваш телефон ще маж достатній заряд батареї")   
+
+    }
+
+
+ if(batterylevel = 29) {
+console.log("Ваш телефон ще має достатній заряд батареї")
+
+}
+
+
+batterylvl > batterylevel
